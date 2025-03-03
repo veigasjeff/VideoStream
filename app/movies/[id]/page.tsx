048fc02a02310d56c60d55efe16be46abf6990f4
ron131977 <110382877+veigasjeff@users.noreply.github.com>
2025-03-03 08:09:29 +0530

    imge updated

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -123,7 +123,7 @@ export default function VideoPage({ params }: Props) {
                   <Film className="w-3 h-3 mr-1" /> Movies
                 </div>
                 <div className="relative w-full aspect-[16/9]">
-                  <Image src={v.thumbnail || "/placeholder.svg"} alt={v.title} quality={90} fill loading="lazy" className="transition-transform group-hover:scale-105 rounded-lg" style={{ objectFit: "cover" }} />
+                  <Image src={v.thumbnail || "/placeholder.svg"} alt={v.title} quality={90} fill loading="lazy" className="transition-transform group-hover:scale-105 rounded-lg" style={{ objectFit: "cover", filter: "contrast(1.1) saturate(1.1) brightness(1.0) hue-rotate(0deg)", }} />
                 </div>
                 <div className="absolute bottom-2 right-2 bg-black/80 text-white px-2 py-1 text-xs rounded-md flex items-center">
                   <Clock className="w-3 h-3 mr-1" /> {v.duration}
